51ed602add88f10d4d88c3130e57fbcf8432dcbc
wijhenke <56547835+wijhenke@users.noreply.github.com>
2020-12-24 17:52:13 +0100

    Add files via upload

--- day16.py
+++ day16.py
@@ -1,3 +1,6 @@
+# Day 16, part 1: Consider the validity of the nearby tickets you scanned.
+# What is your ticket scanning error rate?
+
 from itertools import chain
 
 ranges = ((31,201),(227,951),(49,885),(892,961),(36,248),(258,974),
@@ -8,18 +11,28 @@ ranges = ((31,201),(227,951),(49,885),(892,961),(36,248),(258,974),
             (25,916),(942,966),(39,572),(587,966),(25,834),(858,953),
             (48,534),(544,959),(47,442),(463,969))
 
-
+# create a set with all values that fall within the ranges
 codes = set(chain(*(range(start, end+1) for start, end in ranges)))
-
 
 lines = open('day16a.txt').read().split('\n')
 cF=tF=0
+# for line in lines:
+#     l=line.split(',')
 for line in lines:
-    l=line.split(',')
-
-    for i in l:      
+    for i in line.split(','):
+        
         if int(i) not in codes:
             cF+=int(i)
             tF+=1
         
 print('Nr', tF,'Sum', cF)
+
+
+# part 2: look for the six fields on your ticket that start with the word departure.
+# What do you get if you multiply those six values together?
+
+
+
+# Day 16, part 1: Consider the validity of the nearby tickets you scanned.
+# What is your ticket scanning error rate?
+
