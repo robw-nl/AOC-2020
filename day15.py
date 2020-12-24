2f172875d02c5dbd7735a2c543ecb0c172675cde
wijhenke <56547835+wijhenke@users.noreply.github.com>
2020-12-24 12:12:49 +0100

    Add files via upload

---
+++ day15.py
@@ -0,0 +1,16 @@
+# Day 15: Rambunctious Recitation
+# What will be the 2020th number spoken?
+
+
+def spoken_num(n):
+    spoken=[9,19,1,6,0,5,4]
+    spoken={spoken[i]:i+1 for i in range(len(spoken))} # keep track of how often
+    # print('SPOKEN', spoken)
+    a=0
+    for i in range(len(spoken)+1,n):
+        spoken[a],a = (i, i-spoken[a]) if a in spoken else (i, 0)
+    # print('\n', 'SPOKEN ELSE', i, a, '\n', spoken)
+    return a
+
+print('part1',spoken_num(2020))
+print('part2',spoken_num(30000000))
