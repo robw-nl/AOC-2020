c629fe94f7a448ac75e8ee0dcb24202ee1cd73d4
wijhenke <56547835+wijhenke@users.noreply.github.com>
2020-12-26 14:31:55 +0100

    Add files via upload

--- day16.py
+++ day16.py
@@ -4,25 +4,23 @@ from functools import reduce
 import time,os,re,operator
 
 all_tickets = []
-other_tickets = []
 corr_tickets = []
 all_ranges = set()
-rule_map = {}
+rules_map = {}
 
 for line in open('day16.txt', 'r').read().split('\n'):
     if 'or' in line:
         vals = list(map(int, re.findall(r'(\d+)' , line)))
         all_ranges |= set(range(vals[0] , vals[1] + 1)) | set(range(vals[2] , vals[3] + 1))
-        rule_map[line[:line.find(':') ]] = set(range(vals[0] , vals[1] + 1)) | set(range(vals[2] , vals[3] + 1))
+        rules_map[line[:line.find(':') ]] = set(range(vals[0] , vals[1] + 1)) | set(range(vals[2] , vals[3] + 1))
     elif ',' in line:
         all_tickets.append(list(map(int , line.split(',')))) 
 
 my_ticket = all_tickets[0]
-other_tickets = all_tickets[1:]
 
 is_good=True
 error_rate=0
-for line in other_tickets:
+for line in all_tickets[1:]:
     for j in line:
         if int(j) not in all_ranges:
             error_rate+=int(j)
@@ -31,18 +29,18 @@ for line in other_tickets:
         corr_tickets.append(line)
     is_good=True
 
-print('Part 1: Error rate: {} other tickets: {} corr tickets: {}'.format(error_rate, len(other_tickets), len(corr_tickets)))
+print('Part 1: Error rate: {} other tickets: {} corr tickets: {}'.format(error_rate, len(all_tickets[1:]), len(corr_tickets)))
 
 # part 2: look for the six fields on your ticket that start with the word departure.
 # What do you get if you multiply those six values together?
 
-pos = { i : set(rule_map.keys()) for i in range(len(all_tickets[0])) }
+pos = { i : set(rules_map.keys()) for i in range(len(all_tickets[0])) }
 
 for ticket in all_tickets:
     if any(val not in all_ranges for val in ticket): continue
 
     for idx,val in enumerate(ticket):
-        for rule, value__ in rule_map.items():
+        for rule, value__ in rules_map.items():
             if val not in value__: pos[idx].remove(rule)
 
 visited = [False for _ in range(len(pos))]
@@ -60,4 +58,4 @@ for _ in range(len(pos)):
         if target_field in pos[idx]:
             value.remove(target_field)
 
-print('Part 2 multiplied:', reduce(operator.mul , [ all_tickets[0][i] for i in pos if 'departure' in pos[i].pop()]))
+print('Part 2 multiplied:', reduce(operator.mul, [ all_tickets[0][i] for i in pos if 'departure' in pos[i].pop()]))
