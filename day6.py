98c23915008f2b5c8c7e528971abfe0a60f8f572
wijhenke <56547835+wijhenke@users.noreply.github.com>
2020-12-27 00:06:10 +0100

    Add files via upload

--- day6.py
+++ day6.py
@@ -4,16 +4,16 @@
 
 with open('day6.txt') as f:
     questions = f.read().split('\n')
-    
+
 tot = prod = 0
 qq=''
 
 for q in questions:
     if len(q) > 0:
-        qq = qq + q
+        qq += q
 
     if len(q) == 0:
-        
+
         unique = []
         for char in qq:
             if char not in unique:
@@ -27,11 +27,9 @@ print("Total counts: {}".format(tot))
 # Part two: for each group count nr of questions that EVERYONE answered yes and sum them
 # Took a different approach here
 
-with open("aocday6.txt", "r") as f:
+with open("day6.txt", "r") as f:
     text = f.read().split("\n\n")
     text = [i.splitlines() for i in text]
-
-#print(text)
 
 res = 0
 for group in text:
