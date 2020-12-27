33f94ad2ecd044826b619f632cff86f9e84adb03
wijhenke <56547835+wijhenke@users.noreply.github.com>
2020-12-27 17:40:10 +0100

    Add files via upload

--- day7.py
+++ day7.py
@@ -1,34 +1,20 @@
-# Day 7: Handy Haversacks
-# Part one:
-# This is a tricky one. I think either a tree or graph problem. (....) It's a graph problem. Zero experience
-# with this so I will to build a 'graph' with a tuple, dict with a list that holds the bags containing other bags
+# day 7: the shitty bags containnment problem
 
-bags = dict()
-p=0 # print switch & counter
-
+bags = {}
 for line in open('day7.txt').read().splitlines():
     left_bag, right_bags = line.split(' contain ')
-    
-    if p in range(1,10): print(left_bag, right_bags);
-        
     left_bag = tuple(left_bag.split()[:2])
-    if p in range(1,10): print(left_bag)
-    
-    bags[left_bag] = list()
+    bags[left_bag] = []
     if right_bags != 'no other bags.':
         for right_bag in right_bags.split(', '):
             amount, *colors, _ = right_bag.split()
             bags[left_bag].append((int(amount), tuple(colors)))
-            
-    if p in range(1,10): print(bags[left_bag], amount, '\n');  p+=1
-        
-        
+
 def part_1(bag_color: tuple) -> bool:
     return any(color == target or part_1(color) for _, color in bags[bag_color]) # use _ as color counter
 
 def part_2(bag_color: tuple) -> int:
     return 1 + sum(cnt * part_2(color) for cnt, color in bags[bag_color])
-
 
 target = 'shiny', 'gold'
 
