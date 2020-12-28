8baa1608277df6e2d469c87351fddd0032925f96
wijhenke <56547835+wijhenke@users.noreply.github.com>
2020-12-28 16:53:34 +0100

    Add files via upload

---
+++ day18.py
@@ -0,0 +1,8 @@
+#  # day 18: Evaluate the expression on each line of the homework; what is the sum of the resulting values?
+
+# lines = open('day18.txt').read().strip().split()]
+# print(lines)
+
+
+for line in open('day18.txt').read().splitlines():
+    print(line)
