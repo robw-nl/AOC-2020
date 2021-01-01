40ed21b5f42db46795c04ff1a213c3bcbdc82e8a
wijhenke <56547835+wijhenke@users.noreply.github.com>
2021-01-01 19:27:59 +0100

    Add files via upload

--- day18.py
+++ day18.py
@@ -1,130 +1,78 @@
 # day 18: Evaluate the expression on each line, what is the sum of the resulting values?
 '''
-Find every expression within brackets
-evaluate with simple or complex evaluate function - replace expression incl. brakets by result
-loop until no more brackets (CHECK IF THIS IS CORRECT CONDITION)
-
-Data prep: remove all spaces
-
-Day 2: y="("+str(open("18.txt").read()).replace('*',' ) * ( ').replace('\n',')')
+Find every expression within brackets, evaluate with simple or complex evaluate function - replace expression incl.
+brackets in loop until no more brackets. Evaluate is recursive. Data prep: remove all spaces
 '''
 
-numbers='0123456789'
-
 def eval_complex(s):
-    # find first num, operator and second num, calculate result 
-    # and go to next num op num sequence until eol is reached
-    # make function recursive or call from loop?
-
     sum = l_num = r_num = ops = -1
-    # we don't need the brackets anymore
-    s=s.replace('(','').replace(')','')
-    print('* START eval_complex with ', s)
+    s=s.replace('(','').replace(')','') # don't need brackets now
 
-    while True:
-        for i, j in enumerate(s):
-            # store brackets position
-            # find the first numbers
- 
+    numbers='0123456789'
+    while True: 
+        for i, j in enumerate(s): # store bracket positions, find the first numbers
             if i == len(s):
-                return eval(s)
-
+                return evaluate(s)
             if s.count('*') + s.count('+') <= 1:
                 return eval(s)
-
             if s[i] in numbers and l_num == -1:
                 l_num = i # pos of 1st number
                 z = i
-                while s[z] in numbers: # run through the remaining nnumers
+                while s[z] in numbers: # run through the remaining numbers
                     z+=1 # need a temp var as we can't change i here, the loop will
-
             elif s[i] in '*+' and ops == -1:
                 ops=z # use z as i is now wrong
-
             elif s[i] in numbers and ops > -1:
                 while s[i] in numbers:
                     i+=1
                     if i == len(s):
                         break
                 r_num = i
-
-                # evaluate content between brackets
-                part = s[l_num:r_num]
-
+                part = s[l_num:r_num] # evaluate content between brackets
                 sum = eval_complex(part)
                 s = repl_strbysum(s, str(part), str(sum))
+                s =s
     return 0
 
 def evaluate(s):
-    print('@ START evaluate with ', s)
-    
     for i, _ in enumerate(s):
-
-        # determine if it is a simple or complex expression
-        # simple: eval
-        # complex call eval_complex
-
         try:
-            # simple equation
-            if s.count('*') + s.count('+')  == 1:
-                return eval(s)
-            # only one operator type
-            elif s.count('*') == 0 and s.count('+')  > 0:
-                return eval(s)
-            # only one operator type
-            elif s.count('+') == 0 and s.count('*')  > 0:
-                return eval(s)
-            else:
+            if s.count('+') > 0 and s.count('*')  > 0:
                 return eval_complex(s)
-
+            else:
+                return eval(s)
         except:
             print('@ Error evaluation expression')
     return s
 
 def repl_strbysum(s, strng, part):
-    # use replace() function to replace all occurences of the total
-    # substring by the result. Thia helps to evaluate quicker. Only
-    # replace once or it will mess op the parenthesis
     return s.replace(strng, part, 1)
 
 def parse(s):
-    # find every expression within brackets
-    # evaluate - replace expression incl. brakets by result
-    # loop until no more brackets (CHECK IF THIS IS CORRECT CONDITION)
-
-    print('* START PARSE with ', s)
-    tot_sum = sum = l_br = r_br = 0
+    s=s.replace(' ', '')
     while True:
-        tot_sum+=sum
-        if s.count('*') + s.count('+') == 1:
-            print(s)
-            return eval(s)
-        if s.count('(') + s.count(')') == 0:
-            print(s)
+        sum = l_br = r_br = 0
+        if s.count('(') + s.count(')') == 0: # no brackets, skip processing
             return evaluate(s)
-
-            # return evaluate(s)
+            
         for i, j in enumerate(s):
-            # store brackets position
-            if s[i] == '(':
-                # check following brackets
-                l_br = i
+            if s[i] == '(': # store brackets position
+                l_br = i # check following brackets
             elif s[i] == ')':
-                r_br = i
-                # evaluate content between brackets
+                r_br = i # evaluate content between brackets
                 part = s[l_br:r_br+1]
                 sum = evaluate(part)
                 s = repl_strbysum(s, str(part), str(sum))
-                l_br = r_br = 0
                 break
     return 0
 
 def init():
-    line = open('18.txt').read().replace('\n', ' ').replace(' ', '')
-    print('Line read', line)
+    lines = open('day18.txt').read()
 
-    sum = parse(line)
-    print(sum)
+    s = sum(parse(line) for line in lines.splitlines())
+    t = eval("("+lines.replace('*',')*(').replace('\n',')+(')+"0)")
+
+    print('Part 1: {} - Part 2: {}'.format(s, t))
 
 def main():
     init()
