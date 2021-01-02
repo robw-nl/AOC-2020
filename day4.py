3bbd6a66afa758d9b1ba7dc50525e698c4b3664d
wijhenke <56547835+wijhenke@users.noreply.github.com>
2021-01-02 18:10:51 +0100

    Add files via upload

--- day4.py
+++ day4.py
@@ -3,7 +3,7 @@
 
 with open('day4.txt') as f:
     data = f.read().split('\n\n')
-    
+
 fields = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt']
 tot = 0
 
@@ -23,14 +23,28 @@ for p in data:
     if all(check):
         p = {x.split(':')[0] : x.split(':')[1] for x in re.split('\n| ', p) if ':' in x}
 
-        if ((1920 <= int(p['byr']) <= 2002) and
-            (2010 <= int(p['iyr']) <= 2020) and
-            (2020 <= int(p['eyr']) <= 2030) and
-            (((p['hgt'][-2:] == 'cm') and (150 <= int(p['hgt'][:-2]) <= 193)) or
-            ((p['hgt'][-2:] == 'in') and (59 <= int(p['hgt'][:-2]) <= 76))) and
-            (p['hcl'][0] == '#' and all([x.isalnum() for x in p['hcl'][1:]])) and
-            (p['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']) and
-            (len(p['pid']) == 9 and all([x.isdigit() for x in p['pid']]))):
+        if (
+            1920 <= int(p['byr']) <= 2002
+            and 2010 <= int(p['iyr']) <= 2020
+            and 2020 <= int(p['eyr']) <= 2030
+            and (
+                (
+                    (
+                        (p['hgt'][-2:] == 'cm')
+                        and (150 <= int(p['hgt'][:-2]) <= 193)
+                    )
+                    or (
+                        (p['hgt'][-2:] == 'in')
+                        and (59 <= int(p['hgt'][:-2]) <= 76)
+                    )
+                )
+            )
+            and p['hcl'][0] == '#'
+            and all(x.isalnum() for x in p['hcl'][1:])
+            and p['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+            and len(p['pid']) == 9
+            and all(x.isdigit() for x in p['pid'])
+        ):
             tot += 1
 
 print("Passports with correct formats: {}".format(tot))
