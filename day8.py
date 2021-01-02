7e954eb62b4b7a4a0f79d685e68600a69d955f93
wijhenke <56547835+wijhenke@users.noreply.github.com>
2021-01-02 18:06:08 +0100

    Add files via upload

--- day8.py
+++ day8.py
@@ -1,7 +1,7 @@
 # Day 8: Handheld Haltin
 # part one: fix the infinite loop, determine the accumulator value
 
-f = open("day8.txt").readlines()
+lines = open("day8.txt").readlines()
 
 def part1(lines):
     p = range(1, 15)
@@ -22,7 +22,7 @@ def part1(lines):
             return True, acc
 
 
-print("Accumalator value:",'\t', part1(f)[1])
+print("Accumalator value:",'\t', part1(lines)[1])
 
 
 # part two: accumulator value after programm fix
@@ -31,16 +31,16 @@ p = range(1, 15)
 state = False
 value = 0
 
-for memex, line in enumerate(f):
+for mem_exp, line in enumerate(lines):
     if 'acc' in line:
         continue
     if 'nop' in line: # per instruction replace both with each other
-        f[memex] = line.replace('nop', 'jmp')
+        lines[mem_exp] = line.replace('nop', 'jmp')
     else:
-        f[memex] = line.replace('jmp', 'nop')
-        state, value = part1(f)
+        lines[mem_exp] = line.replace('jmp', 'nop')
+        state, value = part1(lines)
     if state:
         break
-    f[memex] = line
+    lines[mem_exp] = line
 
 print("Fixed accumalator value:", value)
