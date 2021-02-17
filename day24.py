8202a908560488a1981f5f7d4e40b20ed4769765
wijhenke <56547835+wijhenke@users.noreply.github.com>
2021-02-17 17:02:19 +0100

    Add files via upload
    Solution works and is very readable but still suffers from both time and memory complexity. With this setup I think I optimized as much as I could. On teh test set I went from 6.8 minutes to 2 minutes solving both p1 and p2. It should be 1 second though. Next time use sets or dicts!

--- day24.py
+++ day24.py
@@ -1,65 +1,57 @@
-directions = [ ['NW',0,-1],['NE',1,-1],['SW',-1,1],['SE',0,1],['e',1,0],['w',-1,0] ]
+directions = [ [0,-1, 'NW'],[1,-1, 'NE'],[-1,1, 'SW'],[0,1, 'SE'],[1,0, 'e'],[-1,0, 'w'] ]
 
-def find_adj_tiles(stack, tile):
-    x = []
-    for item in directions:
-        x.append([tile[0] + item[1], tile[1] + item[2], tile[2]])
-        for t in stack:
-            if x[-1][:2] == t[:2]:
-                x[-1][2] = t[2]
-                break
-            else:
-                x[-1][2] = False
-    return sum(t[2] for t in x), x
+def add_adj_tiles(stack, tile):
+    adj = [[tile[0] + item[0], tile[1] + item[1], tile[2]] for item in directions]
+    adj.sort()
+    stack.sort()
+    cnt=0
+    for a in adj:
+        a[2] = False
+        for s in stack:
+            if a[:2] == s[:2]:
+                a[2] = s[2]
+                break               
+        cnt += a[2]
+        if tile[2] and a not in stack:
+            stack.insert(0, a)
+    return cnt
 
 def get_coord(tile):
     c1 = c2 = 0
     for item in directions:
-        c1+=tile.count(item[0])*item[1]
-        c2+=tile.count(item[0])*item[2]
-
-    return [c1, c2, False]
+        c1+=tile.count(item[2])*item[0]
+        c2+=tile.count(item[2])*item[1]
+    return [c1, c2, True]
 
 def p2(stack):
     import time
     t0 = time.time()
-
     for c, _ in enumerate(range(100), start=1):
         to_flip = []
-
-        for tile in stack:
-            c_black, adj_tiles = find_adj_tiles(stack, tile)            
-            stack.extend([item for item in adj_tiles if tile[2] and item not in stack])
-
-            if tile[2] and (c_black == 0 or c_black > 2) or (not tile[2] and c_black == 2):
+        for tile in stack:            
+            cnt = add_adj_tiles(stack, tile)             
+            if tile[2] and (cnt == 0 or cnt > 2) or (not tile[2] and cnt == 2):
                 to_flip.append(tile)
-
-        stack.extend([item for item in adj_tiles if tile[2] and item not in stack])
-        for tile in stack:
-            if tile in to_flip:
+        
+        stack.sort()
+        for tile in to_flip:
+            if tile in stack:
                 tile[2] = not tile[2]
-
+         
         print(f'{c} \t{len(stack)} \t{time.time()-t0:.1f}')
     return sum(tile[2] for tile in stack)
 
 def p1(tiles):
     black = []
-    stack = []
-
     for tile in tiles:
-        stack += [get_coord(str(tile))]
-        black.remove(stack[-1]) if stack[-1] in black else black.append(stack[-1])        
-
-    for tile in stack:
-        tile[2] = tile in black
-    
+        x = get_coord(str(tile))
+        black.remove(x) if x in black else black.append(x)
     return black
 
 def main(f):
     tiles = [tile.split() for tile in open(f, 'r').read().split('\n')]
-
     black = p1(tiles)   
     print(f'\np1 {len(black)}, p2 {p2(black)}\n')
 
 if __name__ == '__main__':   
-        main('day24.txt')
+        main('day24ex.txt')
