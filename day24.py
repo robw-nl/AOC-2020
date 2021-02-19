6e18fbb1e41b5d3fe6c36fb7bf6e35e9956ed787
wijhenke <56547835+wijhenke@users.noreply.github.com>
2021-02-19 16:10:56 +0100

    Update day24.py
    And 10 lines shorter now

--- day24.py
+++ day24.py
@@ -1,8 +1,5 @@
 dirs = { 'NW':[0,-1], 'NE':[1,-1], 'SW':[-1,1], 'SE':[0,1],'e':[1,0], 'w':[-1,0] }
 
-def find_adj_tiles(tile):
-    return {(tile[0] + dir[0], tile[1] + dir[1]) for dir in dirs.values()}
-
 def get_coord(tile):
     c1 = c2 = 0
     for key, val in dirs.items():
@@ -13,27 +10,21 @@ def get_coord(tile):
 def p2(stack):
     import time
     t0 = time.time()
-
-    for c, _ in enumerate(range(100), start=1):
+    for _ in range(100):
         adj = set()
         new_stack = set()
 
         for tile in stack:
-            adj.update( find_adj_tiles(tile) )
+            adj.update( (tile[0] + dir[0], tile[1] + dir[1]) for dir in dirs.values() )
 
         for tile in adj:
-            cnt = sum(
-                tuple(a + b for a, b in zip(tile, d)) in stack
-                for d in dirs.values()
-            )
+            cnt = sum(tuple(a + b for a, b in zip(tile, d)) in stack for d in dirs.values())
 
-            if (tile in stack and 0 < cnt <= 2) or (
-                tile not in stack and cnt == 2
-            ):
+            if ( tile in stack and 0 < cnt <= 2) or (tile not in stack and cnt == 2 ):
                 new_stack.add(tile)
         stack = new_stack
 
-    print(f'\n{c} \t{len(stack)} \t{time.time()-t0:.1f}')
+    print(f'\nTimer: {time.time()-t0:.2f}')
     return stack
 
 def p1(tiles):
@@ -47,9 +38,8 @@ def p1(tiles):
     return black
 
 def main(f):
-    tiles = [tile.split() for tile in open(f, 'r').read().split('\n')]
-    black = p1(tiles)   
-    print(f'\nReal input\np1 {len(black)}, p2 {len(p2(black))}\n')
+    black = p1([tile.split() for tile in open(f, 'r').read().split('\n')])   
+    print(f'\nWith puzzle input\np1 {len(black)}, p2 {len(p2(black))}\n')
 
 if __name__ == '__main__':   
         main('day24.txt')
