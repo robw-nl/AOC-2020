c927598d0a41010eb522e1001af3d11f44e5c0e3
wijhenke <56547835+wijhenke@users.noreply.github.com>
2021-03-15 16:39:43 +0100

    Update day1.py
    Shorter version

--- day1.py
+++ day1.py
@@ -2,14 +2,17 @@
 # # Part one: Multiply the two entries that sum to 2020
 # Part two: What is the product of the three entries that sum to 2020
 
-from itertools import combinations
+def main():
+    s = [int(s.strip()) for s in open("day1.txt")]
 
-line = [int(lines.strip()) for lines in open("day1.txt")]
-s1=s2=''
+    for a in s:
+        for b in s:
+            if a+b==2020:
+                print(a*b)
+            for c in s:
+                if a+b+c==2020:
+                    print(a*b*c)
+                    return
 
-for a, b, c in combinations(line, 3):
-    if s1=='' and a+b==2020:
-        s1=("P of {}+{} = {}".format(a, b, a+b))
-    if s2=='' and a+b+c==2020: 
-        s2 = ("S of {}*{}*{} = {}".format(a, b, c, a*b*c))
-print(s1, s2)
+if __name__ == '__main__':   
+        main()
