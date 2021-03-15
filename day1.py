170d40b86856c7119733ac6ca287aaa7377481b3
wijhenke <56547835+wijhenke@users.noreply.github.com>
2021-03-15 16:47:45 +0100

    Update day1.py
    And again a shorter version

--- day1.py
+++ day1.py
@@ -3,7 +3,7 @@
 # Part two: What is the product of the three entries that sum to 2020
 
 def main():
-    s = [int(s.strip()) for s in open("day1.txt")]
+    s = [int(s) for s in open("day1.txt")]
     for a in s:
         for b in s:
             print(a*b) if a+b==2020 else None                
